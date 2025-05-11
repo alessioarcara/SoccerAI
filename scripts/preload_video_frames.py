3a9ac4aad85b0672c379f2c70f7998bdc25e6aa1
Alessio Arcara <alessio.arcara@studio.unibo.it>
2025-05-11 22:12:16 +0000

    Updated preload video frames script

--- scripts/preload_video_frames.py
+++ scripts/preload_video_frames.py
@@ -1,8 +1,12 @@
 from loguru import logger
 from tqdm import tqdm
 
-from soccerai.data.data import load_and_process_metadata, load_and_process_soccer_events
-from soccerai.data.label import _neg_labeling, _pos_labeling
+from soccerai.data.data import (
+    load_and_process_metadata,
+    load_and_process_rosters,
+    load_and_process_soccer_events,
+)
+from soccerai.data.label import get_chains
 from soccerai.data.utils import download_video_frames
 
 if __name__ == "__main__":
@@ -12,17 +16,18 @@ if __name__ == "__main__":
     metadata_df = load_and_process_metadata(
         "/home/soccerdata/FIFA_WorldCup_2022/Metadata"
     )
+    rosters_df = load_and_process_rosters("/home/soccerdata/FIFA_WorldCup_2022/Rosters")
 
-    chain_len = 1
-    pos_chains = _pos_labeling(event_df, chain_len, True)
-    logger.info(f"Generated {len(pos_chains)} positive chains")
-
-    neg_chains = _neg_labeling(
-        event_df, players_df, metadata_df, pos_chains, chain_len, 25.0
+    chains_dict = get_chains(
+        event_df,
+        players_df,
+        metadata_df,
+        rosters_df,
+        skip_challenge_events=False,
+        use_player_pos=True,
     )
-    logger.info(f"Generated {len(neg_chains)} negative chains")
 
-    all_chains = pos_chains + neg_chains
+    all_chains = chains_dict["all_pos_chains"] + chains_dict["all_neg_chains"]
 
     logger.info(f"Downloading video frames for {len(all_chains)} chains...")
 
